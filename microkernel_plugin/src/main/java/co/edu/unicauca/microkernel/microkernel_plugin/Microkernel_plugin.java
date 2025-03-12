c73ad89c213613b201c261919ec45ef7dbababb7
Julian Rojas Lopez <14julianlopez@gmail.com>
2025-03-12 15:42:45 -0500

    Creacion de core, common y plugin

---
+++ microkernel_plugin/src/main/java/co/edu/unicauca/microkernel/microkernel_plugin/Microkernel_plugin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.microkernel.microkernel_plugin;
+
+/**
+ *
+ * @author lopez
+ */
+public class Microkernel_plugin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
